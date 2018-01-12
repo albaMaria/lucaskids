7c92ff2f3152c97727de1d192b1e90708175718f
Alba Mendoza <albamariams97@gmail.com>
2018-01-12 19:52:44 +0000

    new scroll, upload home

--- js/index.js
+++ js/index.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
   $('.slider-home').slick({
     autoplay: true,
-    autoplaySpeed: 2000,
+    autoplaySpeed: 2200,
     dots: true,
     arrows: false,
   });
   AOS.init();
+
+  var scroll = new SmoothScroll('a[href*="#"]');
 })
